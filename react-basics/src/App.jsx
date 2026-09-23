3037bb4efa3e9421e87d3ae4d6743a50fe8b6e34
Tarun Kumar <tarun.25b01010853@abes.ec.in>
2026-09-23 16:10:17 +0530

    Reacts Basics

---
+++ react-basics/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App(){
+  return (
+    <div class="container">
+      <h1> React Basics</h1>
+      <h2>Abes</h2>
+    </div>
+  );
+}
+
+export default App
